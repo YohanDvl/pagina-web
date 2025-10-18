3f88621226d5fd6bac260b95d0cc41eee91f0991
Yohan Dvl <dvlyohan@gmail.com>
2025-10-18 14:39:21 +0000

    brooo en cada una de las vistas debe verse lo del la imagen esa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 
+import HeroSection from '@/components/sections/hero';
 import GallerySection from '@/components/sections/gallery';
 import ContactSection from '@/components/sections/contact';
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <GallerySection />
       <ContactSection />
     </>
